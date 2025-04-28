22dace670ebd8379288ba99da80fbae689e622fe
rizkizulfauzi <rizkizulfauzi18@gmail.com>
2025-04-28 11:14:26 +0700

    commit 10

--- class2.cpp
+++ class2.cpp
@@ -7,4 +7,21 @@ private:
     int panjang;
     int lebar;
     int luas;
+
+public:
+    void input_data()
+    {
+        cout << "Masukan panjang =";
+        cin >> panjang;
+        cout << "Masukan lebar =";
+        cin >> lebar;
+    }
+    void output_data()
+    {
+        cout << "Tampilkan luas =" << luas << endl;
+    }
+    void hitung_luas()
+    {
+        luas = panjang * lebar;
+    }
 };
