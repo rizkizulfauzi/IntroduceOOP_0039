cdbd641d224bc1c71444a7c774434a8a926f8dfd
rizkizulfauzi <rizkizulfauzi18@gmail.com>
2025-04-28 11:04:56 +0700

    commit 4

--- class.cpp
+++ class.cpp
@@ -9,4 +9,19 @@ public:
 
 private:
     int harga_mobil;
+
+public:
+    void input_data()
+    {
+        cout << "Masukan nama mobil =";
+        cin >> nama_mobil;
+        cout << "Masukan tipe mobil =";
+        cin >> tipe_mobil;
+        cout << "Masukan harga mobil =";
+        cin >> harga_mobil;
+    }
+    void output_data()
+    {
+        cout << "Harga mobil =" << harga_mobil << endl;
+    }
 };
